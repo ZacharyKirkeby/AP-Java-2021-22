e63f3b8cf133fd203da58cccd8101010ed032efe
Zachary Kirkeby <95650247+ZacharyKirkeby@users.noreply.github.com>
2023-01-13 10:52:53 -0600

    Lab work

---
+++ JavaLabs/ObjectLab/Lab 2.2 My First Class/Greeter.java
@@ -0,0 +1,25 @@
+public class Greeter{/* Write the Greeter class in this file.
+
+ * This class should have 2 methods in it.
+ * 
+ * sayHi method
+ * Returns nothing
+ * Takes no paramters
+ * Outputs the message "Hello" using System.out.println
+ * Note: this method is one of the few methods that should use System.out.println
+ * 
+ * greet method
+ * Returns nothing
+ * Takes a String parameter called name
+ * Outputs the message "Hello [NAME]" where [NAME] is the value of the name parameter
+ * Note: this method is one of the few methods that should use System.out.println
+ */
+public void sayHi(){
+  System.out.println("Hello");
+}
+public void greet(String s){
+  System.out.println("Hello " + s);
+}
+
+
+}
---
+++ JavaLabs/ObjectLab/Lab 2.2 My First Class/Main (3).java
@@ -0,0 +1,15 @@
+/* DO NOT CHANGE ANYTHING IN THIS FILE
+ * If you are getting an error in this file, it is becuase
+ * of a mistake in your Greeter.java file!
+ */
+ 
+ class Main {
+  public static void main(String[] args) {
+       // Instantiate a Greeter Object
+    Greeter g = new Greeter();
+    
+    // Invoke the Greeter object's methods
+    g.sayHi();
+    g.greet("Cam Newton");
+  }
+}
